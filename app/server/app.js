7c8ae40e7803b17dde07eec71ad4fbfc5366eaed
Chris Bates-Keegan <chris@evansfinch.co.uk>
2016-08-15 12:46:23 +0100

    - ADDED: Support for native JS to build process - CONVERTED: app.coffee to app.js ES6.

---
+++ app/server/app.js
@@ -0,0 +1,80 @@
+'use strict';
+
+//
+// Includes
+//
+
+var express = require('express');
+var json = require('express-json');
+var methodOverride = require('method-override');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var Model = require('./model/index');
+var Routes = require('./routes/index');
+var Config = require('./config');
+
+//
+// Export Express
+//
+
+var app = module.exports = express();
+
+//
+// Configuration
+//
+var configureDevelopment = () => {
+  Config.env = 'dev';
+  app.set('db-uri', `mongodb://${Config.mongoUrl.dev}/${Config.app.code}-dev`);
+  app.use(morgan('short'));
+  app.set('port', Config.listenPort.dev);
+};
+
+var configureProduction = () => {
+  Config.env = 'prod';
+  app.set('db-uri', `mongodb://${Config.mongoUrl.dev}/${Config.app.code}-prod`);
+  app.use(morgan('short'));
+  app.set('port', Config.listenPort.prod);
+};
+
+var configureTest = () => {
+  Config.env = 'test';
+  app.set('db-uri', `mongodb://${Config.mongoUrl.test}/${Config.app.code}-test`);
+  app.use(morgan('short'));
+  app.set('port', Config.listenPort.test);
+};
+
+var configureApp = env => {
+  app.enable('trust proxy', 1);
+  app.use(json());
+  app.use(methodOverride());
+
+  switch (env) {
+    default:
+    case 'development': {
+      configureDevelopment();
+    }
+      break;
+    case 'production': {
+      configureProduction();
+    }
+      break;
+    case 'test': {
+      configureTest();
+    }
+      break;
+  }
+};
+
+configureApp(app.get('env'));
+
+//
+// Connect to MongoDb and Init
+//
+app.db = mongoose.connect(app.get('db-uri'));
+app.db.connection.on('connected', () => {
+  console.log(`${Config.app.title} listening on port %d in %s mode.`, app.get('port'), app.settings.env);
+
+  Model.init();
+  Routes.init(app);
+  app.server = app.listen(app.set('port'));
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,70 @@
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-var gulp          = require('gulp'),
-  coffee          = require('gulp-coffee'),
-  jshint          = require('gulp-jshint'),
-  rename          = require('gulp-rename'),
-  clean           = require('gulp-clean'),
-  concat          = require('gulp-concat');
+//
+// Includes
+//
 
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-gulp.task('scripts', function() {
+var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var eslint = require('gulp-eslint');
+var clean = require('gulp-clean');
+
+//
+// Scripts
+//
+
+gulp.task('coffee', function() {
   return gulp.src(['app/server/**/*.coffee'])
     .pipe(coffee())
-    .pipe(gulp.dest('deploy'))
+    .pipe(gulp.dest('deploy'));
 });
 
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
+gulp.task('js', function() {
+  return gulp.src('app/server/**/*.js')
+		.pipe(eslint())
+		.pipe(eslint.format())
+		// .pipe(eslint.failAfterError())
+		.pipe(gulp.dest('deploy'));
+});
 
-gulp.task('mongroup', function(){
+gulp.task('scripts', function() {
+  return gulp.start(['coffee', 'js']);
+});
+
+//
+// Static resources
+//
+
+gulp.task('mongroup', function() {
   return gulp.src('app/server/mongroup.*')
-    .pipe(gulp.dest('deploy'))
+    .pipe(gulp.dest('deploy'));
 });
-gulp.task('json', function(){
+gulp.task('json', function() {
   return gulp.src('app/server/*.json')
-    .pipe(gulp.dest('deploy'))
+    .pipe(gulp.dest('deploy'));
 });
 
 gulp.task('resources', function() {
-  return gulp.start(['mongroup','json']);
+  return gulp.start(['mongroup', 'json']);
 });
 
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
+//
+// Commands
+//
+
 gulp.task('watch', ['clean'], function() {
-  gulp.start('scripts','resources');
+  gulp.start('scripts', 'resources');
 
   // Watch Scripts
-  gulp.watch(['app/**/*.coffee'], ['scripts']);
+  gulp.watch(['app/**/*.coffee'], ['coffee']);
+  gulp.watch(['app/server/**/*.js'], ['js']);
   // Watch Resources
   gulp.watch('app/server/nodemon.json', ['nodemon']);
 });
 
-
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
 gulp.task('clean', function() {
-    return gulp.src(['deploy/*'], {read: false})
-    .pipe(clean());
+  return gulp.src(['deploy/*'], {read: false})
+  .pipe(clean());
 });
 
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-gulp.task('build',['clean'], function() {
-    return gulp.start('scripts','resources');
+gulp.task('build', ['clean'], function() {
+  return gulp.start('scripts', 'resources');
 });
