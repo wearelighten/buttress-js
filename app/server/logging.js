1ce83d8b1b41aea35bd36f484c665ec5ac55c30b
Chris Bates-Keegan <chris@evansfinch.co.uk>
2016-08-17 16:21:49 +0100

    - UPDATED: Added log level framework.

--- app/server/logging.js
+++ app/server/logging.js
@@ -15,29 +15,65 @@
 
 const logFormat = Date.ISO8601_DATETIME;
 var logPrefix = module.exports.logPrefix = () => {
-  Date.create().format(logFormat);
+  return Date.create().format(logFormat);
 };
+
+var LogLevel = {
+  NONE: 0,
+  ERR: 1,
+  WARN: 2,
+  INFO: 3,
+  VERBOSE: 4,
+  DEFAULT: 3
+};
+
+module.exports.Constants = {
+  LogLevel: LogLevel
+};
+var _logLevel = LogLevel.INFO;
 
 /**
  *
- * @param {string} string - log entry
+ * @param {string} log - log entry
+ * @param {integer} level - level to log at
  * @private
  */
-function _log(string) {
-  console.log(string);
+function _log(log, level) {
+  if (_logLevel >= level) {
+    console.log(log);
+  }
 }
 
 /**
  * STANDARD LOGGING
  */
 
+module.exports.setLogLevel = level => {
+  _logLevel = level;
+};
+
 /**
  * @param {string} log - Text to log
+ * @param {integer} level - level to log at
+ */
+module.exports.stdLog = (log, level) => {
+  level = level || LogLevel.DEFAULT;
+  _log(`${logPrefix()} - ${log}`, level);
+};
+
+/**
+ * PROMISE LOGGING
+ */
+
+/**
+ * @param {string} log - Text to log
+ * @param {integer} level - level to log at
  * @return {function(*)} - returns a function for chaining into a promise
  */
-module.exports.log = log => {
+module.exports.log = (log, level) => {
+  level = level || LogLevel.DEFAULT;
   return res => {
-    _log(`${logPrefix()} - ${log}: ${res}`);
+    _log(`${logPrefix()} - ${log}: ${res}`, level);
     return res;
   };
 };
@@ -45,12 +81,14 @@ module.exports.log = log => {
 /**
  * @param {string} log - Text to log
  * @param {*} val - value to test `res` against
+ * @param {integer} level - level to log at
  * @return {function(*)} - returns a function for chaining into a promise
  */
-module.exports.logIf = (log, val) => {
+module.exports.logIf = (log, val, level) => {
+  level = level || LogLevel.DEFAULT;
   return res => {
     if (val === res) {
-      _log(`${logPrefix()} - ${log}: ${res}`);
+      _log(`${logPrefix()} - ${log}: ${res}`, level);
     }
     return res;
   };
@@ -59,12 +97,14 @@ module.exports.logIf = (log, val) => {
 /**
  * @param {string} log - Text to log
  * @param {*} val - value to test `res` against
+ * @param {integer} level - level to log at
  * @return {function(*)} - returns a function for chaining into a promise
  */
-module.exports.logIfNot = (log, val) => {
+module.exports.logIfNot = (log, val, level) => {
+  level = level || LogLevel.DEFAULT;
   return res => {
     if (val !== res) {
-      _log(`${logPrefix()} - ${log}: ${res}`);
+      _log(`${logPrefix()} - ${log}: ${res}`, level);
     }
     return res;
   };
@@ -77,11 +117,13 @@ module.exports.logIfNot = (log, val) => {
 /**
  * @param {string} log - Text to log
  * @param {string} prop - Name of the `res` property to log
+ * @param {integer} level - level to log at
  * @return {function(*)} - returns a function for chaining into a promise
  */
-module.exports.logProp = (log, prop) => {
+module.exports.logProp = (log, prop, level) => {
+  level = level || LogLevel.DEFAULT;
   return res => {
-    _log(`${logPrefix()} - ${log}: ${res[prop]}`);
+    _log(`${logPrefix()} - ${log}: ${res[prop]}`, level);
     return res;
   };
 };
@@ -90,12 +132,14 @@ module.exports.logProp = (log, prop) => {
  * @param {string} log - Text to log
  * @param {string} prop - Name of the `res` property to log
  * @param {*} val - value to test `res` against
+ * @param {integer} level - level to log at
  * @return {function(*)} - returns a function for chaining into a promise
  */
-module.exports.logPropIf = (log, prop, val) => {
+module.exports.logPropIf = (log, prop, val, level) => {
+  level = level || LogLevel.DEFAULT;
   return res => {
     if (val === res[prop]) {
-      _log(`${logPrefix()} - ${log}: ${res[prop]}`);
+      _log(`${logPrefix()} - ${log}: ${res[prop]}`, level);
     }
     return res;
   };
@@ -105,12 +149,14 @@ module.exports.logPropIf = (log, prop, val) => {
  * @param {string} log - Text to log
  * @param {string} prop - Name of the `res` property to log
  * @param {*} val - value to test `res` against
+ * @param {integer} level - level to log at
  * @return {function(*)} - returns a function for chaining into a promise
  */
-module.exports.logPropIfNot = (log, prop, val) => {
+module.exports.logPropIfNot = (log, prop, val, level) => {
+  level = level || LogLevel.DEFAULT;
   return res => {
     if (val !== res[prop]) {
-      _log(`${logPrefix()} - ${log}: ${res[prop]}`);
+      _log(`${logPrefix()} - ${log}: ${res[prop]}`, level);
     }
     return res;
   };
@@ -122,11 +168,13 @@ module.exports.logPropIfNot = (log, prop, val) => {
 
 /**
  * @param {string} log - Text to log
+ * @param {integer} level - level to log at
  * @return {function(*)} - returns a function for chaining into a promise
  */
-module.exports.logArray = log => {
+module.exports.logArray = (log, level) => {
+  level = level || LogLevel.DEFAULT;
   return res => {
-    console.log(`${logPrefix()} - ${log}: ${res.length}`);
+    console.log(`${logPrefix()} - ${log}: ${res.length}`, level);
     res.forEach(r => {
       _log(r);
     });
@@ -137,11 +185,13 @@ module.exports.logArray = log => {
 /**
  * @param {string} log - Text to log
  * @param {string} prop - Name of the `res[]` property to log
+ * @param {integer} level - level to log at
  * @return {function(*)} - returns a function for chaining into a promise
  */
-module.exports.logArrayProp = (log, prop) => {
+module.exports.logArrayProp = (log, prop, level) => {
+  level = level || LogLevel.DEFAULT;
   return res => {
-    console.log(`${logPrefix()} - ${log}: ${res.length}`);
+    console.log(`${logPrefix()} - ${log}: ${res.length}`, level);
     res.forEach(r => {
       _log(r[prop]);
     });
