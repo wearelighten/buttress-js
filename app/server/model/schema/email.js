33dd22682855e8be54fc1efb21b6261da510c227
Chris Bates-Keegan <chris@evansfinch.co.uk>
2016-10-07 15:39:26 +0100

    - ADDED: email schema basics

--- app/server/model/schema/email.js
+++ app/server/model/schema/email.js
@@ -12,9 +12,8 @@
  */
 
 const mongoose = require('mongoose');
-const Model = require('../');
 const Logging = require('../../logging');
-
+require('sugar');
 /**
  * Constants
  */
@@ -22,17 +21,52 @@ const Logging = require('../../logging');
 var constants = {
 };
 
-// Logging.log(Model.initModel('Appauth'));
-// Logging.log(Model.Schema.Appauth);
-
 /**
  * Schema
  */
 var schema = new mongoose.Schema();
 schema.add({
+  headers: {
+    from: String,
+    to: String,
+    subject: String
+  },
+  html: String,
+  sendOn: {
+    type: Date,
+    default: null
+  },
+  sent: {
+    type: Boolean,
+    default: false
+  },
+  sentOn: {
+    type: Date,
+    default: null
+  }
 });
 
 var ModelDef = null;
+
+/**
+ * @param {Object} body - body passed through from a POST request
+ * @return {Promise} - returns a promise that is fulfilled when the database request is completed
+ */
+schema.statics.add = body => {
+  var email = new ModelDef({
+    headers: {
+      from: body.from,
+      to: body.to,
+      subject: body.subject
+    },
+    html: body.html,
+    sendOn: Date.create(body.sendOn)
+  });
+
+  Logging.log(email.headers, Logging.Constants.LogLevel.DEBUG);
+
+  return email.save();
+};
 
 /**
  * @type {{constants: {}, schema: {}, model: {}}}
