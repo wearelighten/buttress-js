3b5c6dedf170aed4321d2d4a83494c034e42447a
Chris Bates-Keegan <chris@evansfinch.co.uk>
2017-07-04 17:27:27 +0100

    - CHANGED: expect vector-add'ed objects that have one to have a property '.id' rather than '._id'. Transform before inserting into db.

--- app/server/model/shared.js
+++ app/server/model/shared.js
@@ -95,7 +95,12 @@ let _doUpdate = (entity, body, pathContext) => {
       }
       case 'vector-add': {
         const vector = entity.get(body.path);
+        if (body.value instanceof Object && body.value.id) {
+          body.value._id = body.value.id;
+          delete body.value.id;
+        }
         vector.push(body.value);
+
         response = vector[vector.length - 1];
         if (response && response.toObject) {
           response = response.toObject();
