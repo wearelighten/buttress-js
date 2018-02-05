3320fb7a29fc5c8598bafa33d2aad4b7fef51c39
Tom Cahill <tomc@wearelighten.co.uk>
2018-02-05 16:41:06 +0000

    - ADDED: Namespace grouping per application for worker sockets
    - TWEAK: Added native mongodb driver collection to app/token

--- app/server/bootstrap-socket.js
+++ app/server/bootstrap-socket.js
@@ -16,9 +16,12 @@ const Express = require('express');
 const sio = require('socket.io');
 const sioRedis = require('socket.io-redis');
 const sioEmitter = require('socket.io-emitter');
-const NRP = require('node-redis-pubsub');
+
 const Config = require('./config');
+const Model = require('./model');
 const Logging = require('./logging');
+const MongoClient = require('mongodb').MongoClient;
+const NRP = require('node-redis-pubsub');
 
 /* ********************************************************************************
  *
@@ -30,21 +33,9 @@ const _workers = [];
 
 /* ********************************************************************************
  *
- * WORKERS
+ * HELPERS
  *
  **********************************************************************************/
-const __spawnWorkers = () => {
-  Logging.log(`Spawning ${processes} Socket Workers`);
-
-  const __spawn = idx => {
-    _workers[idx] = cluster.fork();
-  };
-
-  for (let x = 0; x < processes; x++) {
-    __spawn(x);
-  }
-};
-
 const __indexFromIP = (ip, spread) => {
   let s = '';
   for (let i = 0, _len = ip.length; i < _len; i++) {
@@ -56,20 +47,86 @@ const __indexFromIP = (ip, spread) => {
   return Number(s) % spread;
 };
 
+/* ********************************************************************************
+ *
+ * MONGODB
+ *
+ **********************************************************************************/
+const POOL_SIZE = 10;
+const __nativeMongoConnect = app => {
+  const mongoUrl = `mongodb://${Config.mongoDb.url}`;
+  const dbName = `${Config.app.code}-${Config.env}`;
+  return MongoClient.connect(mongoUrl, {poolSize: POOL_SIZE, native_parser: true})
+  .then(client => {
+    return client.db(dbName);
+  });
+};
+const __initMongoConnect = () => {
+  return __nativeMongoConnect()
+  .then(db => {
+    Model.init(db);
+    return db;
+  })
+  .catch(e => Logging.logError(e));
+};
+
+/* ********************************************************************************
+ *
+ * WORKERS
+ *
+ **********************************************************************************/
+const __spawnWorkers = rooms => {
+  Logging.log(`Spawning ${processes} Socket Workers`);
+
+  const __spawn = idx => {
+    _workers[idx] = cluster.fork();
+    _workers[idx].send({'buttress:initAppNamespace': rooms});
+  };
+
+  for (let x = 0; x < processes; x++) {
+    __spawn(x);
+  }
+
+  net.createServer({pauseOnConnect: true}, connection => {
+    const worker = _workers[__indexFromIP(connection.remoteAddress, processes)];
+    worker.send('buttress:connection', connection);
+  }).listen(Config.listenPorts.sock);
+};
+
+const __initSocketNamespace = (io, name) => {
+  const namespace = io.of(`/${name}`);
+
+  namespace.on('connect', socket => {
+    console.log(socket.handshake.query.token);
+    console.log(`Connection to ${name}`);
+
+    // Handle room assignment based on token
+
+  });
+
+  return namespace;
+};
+
 const __initWorker = () => {
   const app = new Express();
   const server = app.listen(0, 'localhost');
   const io = sio(server);
+  const namespace = [];
   io.origins('*:*');
   io.adapter(sioRedis(Config.redis));
 
-  process.on('message', (message, connection) => {
-    if (message !== 'sticky-session:connection') {
+  process.on('message', (message, input) => {
+    if (message === 'buttress:connection') {
+      const connection = input;
+      server.emit('connection', connection);
+      connection.resume();
       return;
     }
-
-    server.emit('connection', connection);
-    connection.resume();
+    if (message['buttress:initAppNamespace']) {
+      message['buttress:initAppNamespace'].forEach(name => {
+        namespace.push(__initSocketNamespace(io, name));
+      });
+    }
   });
 };
 
@@ -81,8 +138,10 @@ const __initWorker = () => {
 const __initMaster = express => {
   const emitter = sioEmitter(Config.redis);
   const nrp = new NRP(Config.redis);
+  const publicAppIds = [];
 
   const isPrimary = Config.sio.app === 'primary';
+
   if (isPrimary) {
     Logging.logDebug(`Primary Master`);
     nrp.on('activity', data => {
@@ -91,12 +150,20 @@ const __initMaster = express => {
     });
   }
 
-  __spawnWorkers();
-
-  net.createServer({pauseOnConnect: true}, connection => {
-    const worker = _workers[__indexFromIP(connection.remoteAddress, processes)];
-    worker.send('sticky-session:connection', connection);
-  }).listen(Config.listenPorts.sock);
+  __initMongoConnect()
+  .then(db => {
+    // Fetch all applications
+    Model.App.findAll().toArray((err, apps) => {
+      if (err) throw new Error(err);
+      // Create Application rooms with user groups
+      apps.forEach(app => {
+        // TODO: Replace with correct name/token
+        publicAppIds.push(Model.App.getPublicUID(app.name, app._token));
+      });
+      // Create workers, pass through room map
+      __spawnWorkers(publicAppIds);
+    });
+  });
 };
 
 /* ********************************************************************************
